d92cad7f6128d62da7a8f95a27d24851ebe6e693
hwilsonnn <harry@wilson-family.com>
2023-07-22 12:01:52 +1000

    better implementation of hashrouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,12 @@
-import { RouterProvider, createHashRouter } from "react-router-dom"
 import Root from "./routes"
-import ErrorPage from "./routes/ErrorPage"
 import background from "./assets/bgImage.svg"
-import Resume from "./routes/Resume/Resume"
-import { resumeLoader } from "./routes/Resume/resumeLoader"
-
-const router = createHashRouter([
-  {
-    path: "/",
-    element: <Root />,
-    errorElement: <ErrorPage />,
-    children: [{ loader: resumeLoader, path: "/resume", element: <Resume /> }]
-  }
-])
 
 const App = () => (
   <div
     className="flex h-screen w-full flex-row justify-center overflow-x-hidden"
     style={{ backgroundImage: `url(${background})` }}
   >
-    <RouterProvider router={router} />
+    <Root />
   </div>
 )
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,24 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
-import App from "./App.tsx"
 import "./index.css"
+
+import { RouterProvider, createHashRouter } from "react-router-dom"
+import Root from "./routes"
+import ErrorPage from "./routes/ErrorPage"
+import Resume from "./routes/Resume/Resume"
+import { resumeLoader } from "./routes/Resume/resumeLoader"
+
+const router = createHashRouter([
+  {
+    path: "/",
+    element: <Root />,
+    errorElement: <ErrorPage />,
+    children: [{ loader: resumeLoader, path: "/resume", element: <Resume /> }]
+  }
+])
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 )
